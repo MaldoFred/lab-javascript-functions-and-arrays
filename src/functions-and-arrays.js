1f835a768b6a233ac28c69a03d7985cc827ba932
MaldoFred <114544297+MaldoFred@users.noreply.github.com>
2022-10-12 18:17:04 -0500

    realiza tarea

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,19 +1,52 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if(num1 > num2) {
+    return num1;
+  }  else {
+    return num2;
+  }
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+  if(!arr.length){
+    return null;
+  }
+let longest = 0;
+let longest2;
+for(let i=0; i<arr.length; i++){
+  if(arr[i].length > longest) {
+    longest = arr[i].length;
+    longest2 = arr[i];
+    
+  } else if  (arr[i].length ===0){
+    longest2 = null;
+  }
+    
+  }return longest2;
+}
+
+findLongestWord(words); 
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr) {
+  let sum = 0;
+  for(let i = 0;i<arr.length; i++){
+    
+    sum += arr[i];
+    
+  } 
+  return sum;
+  }
+  sumNumbers(numbers);
 
 
 
@@ -26,13 +59,34 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arr) {
+  if(!arr.length){
+      return null;
+  }
+  let avg = 0;
+  let sum = 0;
+  for(let i = 0;i<arr.length; i++) {
+      sum += arr[i];
+      avg = sum/arr.length;
+  } return avg;
+  }
 
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
 
-function averageWordLength() { }
+function averageWordLength(arr) { 
+  let sum = 0;
+  if(!arr.length){
+    return null;
+  }
+  
+  for(let i = 0; i<arr.length; i++){
+    sum += arr[i].length;
+  } 
+  let avg = sum/arr.length;
+  return avg;
+  } 
 
 // Bonus - Iteration #4.1
 function avg() {}
@@ -59,7 +113,17 @@ function uniquifyArray() {}
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(arr, value) {
+  if(!arr.length){
+     return null;
+ }
+  if(arr.indexOf(value) > -1) {
+   return true;
+  } else {
+   return false;
+  }
+ }
+ 
 
 
 
